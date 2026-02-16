92bb83d7b1d9fee1fc1376ec98c3821af935e5ce
Thomas Wagener <thomas.wagener@fischantiquariat.de>
2026-02-16 09:16:09 +0100

    First version of smart_cms component

---
+++ chapter03/smart_cms/smartcms_test_host/src/main.rs
@@ -0,0 +1,57 @@
+use wasmtime::component::bindgen;
+
+bindgen!({
+    path: "./smart_cms.wit",
+    world: "app",
+});
+
+#[derive(Default)]
+struct KeyValue {
+    mem: std::collections::HashMap<String, String>,
+}
+
+impl component::smartcms::kvstore::Host for KeyValue {
+    fn get(&mut self, key: String) -> String {
+        self.mem.get(&key).cloned().unwrap()
+    }
+
+    fn set(&mut self, key: String, value: String) {
+        self.mem.insert(key, value);
+    }
+}
+
+struct State {
+    key_value: KeyValue,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            key_value: KeyValue::default(),
+        }
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+fn main() {
+    let mut config = wasmtime::Config::default();
+    config.wasm_component_model(true);
+
+    let engine = wasmtime::Engine::new(&config).unwrap();
+    let mut store = wasmtime::Store::new(&engine, State::new());
+
+    let component = wasmtime::component::Component::from_file(&engine, "guest.wasm").unwrap();
+    let mut linker = wasmtime::component::Linker::new(&engine);
+    component::smartcms::kvstore::add_to_linker(&mut linker, |state: &mut State| {
+        &mut state.key_value
+    })
+    .unwrap();
+
+    let app = App::instantiate(&mut store, &component, &linker).unwrap();
+    println!("{:?}", app.call_run(&mut store).unwrap());
+}
