b20a9385708343f7327f40fdfc867b93957b635e
Thomas Wagener <thomas.wagener@gls-group.com>
2026-02-27 17:09:11 +0100

    Last wasmcloud example of chapter 6 with issues in wadm.yaml

--- chapter06/smart-cms/src/lib.rs
+++ chapter06/smart-cms/src/lib.rs
@@ -1,13 +1,67 @@
-use wasmcloud_component::http;
+use std::io::Read;
 
+use thomastaylor312::ollama::generate::{generate, Request};
+use wasmcloud_component::http;
+use wasmcloud_component::wasi::keyvalue::*;
+
+wit_bindgen::generate!({ generate_all });
 struct Component;
 
 http::export!(Component);
 
 impl http::Server for Component {
     fn handle(
-        _request: http::IncomingRequest,
+        request: http::IncomingRequest,
     ) -> http::Result<http::Response<impl http::OutgoingBody>> {
-        Ok(http::Response::new("Hello from Rust!\n"))
+        let (parts, mut body) = request.into_parts();
+        match parts.uri.path() {
+            "/api/create" => {
+                let bucket = store::open("default").unwrap();
+                let mut buf = Vec::new();
+                body.read_to_end(&mut buf).unwrap();
+                let body = String::from_utf8(buf).unwrap();
+                let mut lines = body.lines();
+                let story_name = lines.next().unwrap_or("Unnamed Story").trim();
+                let story_content = lines.collect::<Vec<_>>().join("\n");
+                bucket.set(story_name, story_content.as_bytes()).unwrap();
+                Ok(http::Response::new(format!(
+                    "Story '{}'i\n created successfully!\n",
+                    story_name
+                )))
+            }
+            "/api/retrieve" => {
+                let bucket = store::open("default").unwrap();
+                let mut buf = Vec::new();
+                body.read_to_end(&mut buf).unwrap();
+                let story_name = String::from_utf8(buf).unwrap().trim().to_string();
+                match bucket.get(&story_name).unwrap() {
+                    Some(content) => {
+                        let story_content = String::from_utf8(content).unwrap();
+
+                        Ok(http::Response::new(format!("{story_content}\n")))
+                    }
+                    None => Ok(http::Response::new("Story not found.\n".to_string())),
+                }
+            }
+            "/api/generate" => {
+                let prompt = "Once upon a time".to_string();
+                let generated_story = generate(&Request {
+                    prompt: prompt.clone(),
+                    images: None,
+                })
+                .unwrap();
+                let story = format!("{} {}", prompt, generated_story.response);
+
+                let bucket = store::open("default").unwrap();
+                let count = atomics::increment(&bucket, "counter", 1).unwrap();
+                let story_name = format!("generated{}", count);
+                bucket.set(&story_name, story.clone().as_bytes()).unwrap();
+                Ok(http::Response::new(format!(
+                    "Generated Story #{}:\n{}\n",
+                    count, story
+                )))
+            }
+            _ => Ok(http::Response::new("Invalid Route!\n".to_string())),
+        }
     }
 }
